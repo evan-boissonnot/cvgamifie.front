c9968149eb2503a0aff867cf50e9414116a655a4
Evan Boissonnot <evan@boissonnot.fr>
2020-10-09 14:18:20 +0200

    evo: adding game object and children, and connect them into service

--- front-end/src/app/shared/services/game/game.service.ts
+++ front-end/src/app/shared/services/game/game.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Game } from '../../../core/models/game';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  selectOnResume(id: number): Observable<Game> {
+    return this.httpClient.get<Game>(`${environment.apis.games.url}/game/${id}`);
+  }
 }
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apis: {
+    games: {
+      url: 'https://379ad5ea-e116-423b-bda6-9ecbe926e9ef.mock.pstmn.io/game'
+    }
+  }
 };
 
 /*
